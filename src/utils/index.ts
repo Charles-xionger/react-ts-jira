5a6c9b4ff44c65739da65cdd7da1f35438d80253
‘Charles-xionger’ <77594654+Charles-xionger@users.noreply.github.com>
2021-12-07 05:23:16 +0800

    chore: any->unknown

---
+++ src/utils/index.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+
+// 在一个函数中，改变传入的对象本身是不好的
+export const cleanObject = (object: object) => {
+  // Object.assign({}, object)
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    // @ts-ignore
+    const value = result[key];
+    // 存在特殊情况， 无法判断 0
+    if (isFalsy(value)) {
+      // @ts-ignore
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// 用泛型来规范类型
+export const useDebounce = (value: unknown, delay?: number): any => {
+  // 值是响应式的
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在 value 变化后， 设置一个定时器
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
