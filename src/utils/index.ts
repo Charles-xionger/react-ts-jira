f1afbcce4566a24d01cebb381a8c8b13146ff80b
‘Charles-xionger’ <77594654+Charles-xionger@users.noreply.github.com>
2021-12-07 06:17:33 +0800

    chore: 给useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -25,7 +25,7 @@ export const useMount = (callback: () => void) => {
 };
 
 // 用泛型来规范类型
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   // 值是响应式的
   const [debouncedValue, setDebouncedValue] = useState(value);
 
