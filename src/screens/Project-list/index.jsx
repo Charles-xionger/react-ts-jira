329573ea63e66fbd817d1ed5832d332b1c9be1c5
‘Charles-xionger’ <77594654+Charles-xionger@users.noreply.github.com>
2021-12-06 20:33:33 +0800

    feat: 添加useMount和useDebounce两个hook

--- src/screens/Project-list/index.jsx
+++ src/screens/Project-list/index.jsx
@@ -15,24 +15,44 @@ export const ProjectListScreen = () => {
   const [users, setUsers] = useState([])
 
   const [list,setList] = useState([])
-
+  const debouncedParam = useDebounce(param, 5000)
   useEffect(() => {
     fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
       if(response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if(response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam}/>
     <List list={list} users={users}/>
   </div>
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  },[])
+}
+
+export const useDebounce = (value, delay) => {
+  // 值是响应式的
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    // 每次在 value 变化后， 设置一个定时器
+    const timer = setTimeout(() => setDebouncedValue(value), delay)
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timer)
+  }, [value, delay])
+
+  return debouncedValue
 }
